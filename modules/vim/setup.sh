9809b692c68036663f8463dd46cea4d2083fe8ea
Marco Neumann <marco@crepererum.net>
2016-02-01 18:50:06 +0100

    [vim] update to latest neovim changes
    Signed-off-by: Marco Neumann <marco@crepererum.net>

--- modules/vim/setup.sh
+++ modules/vim/setup.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env sh
 
 # cleanup
-rm -rIf ~/.vimrc ~/.viminfo ~/.vim
+rm -rIf ~/.vimrc ~/.viminfo ~/.vim ~/.config/nvim
 
-# link .vimrc and .nvimrc
+# create nvim config dir
+mkdir -p ~/.config/nvim
+
+# link .vimrc and nvimrc
 ln -s $mdir/vimrc ~/.vimrc
-ln -s ~/.vimrc ~/.nvimrc
+ln -s ~/.vimrc ~/.config/nvim/init.vim
 
 # create view dirs
 mkdir -p ~/.vim/view
-mkdir -p ~/.nvim/view
+mkdir -p ~/.config/nvim/view
 
-# install vim-plug
+# install vim-plug (vim)
 mkdir -p ~/.vim/autoload
 curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qa
+
+# install vim-plug (nvim)
+mkdir -p ~/.config/nvim/autoload
+curl -fLo ~/.config/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim +PlugInstall +qa
 
 # install YouCompleteMe
 #cd ~/.vim/bundle/YouCompleteMe
