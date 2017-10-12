fcb0ee0d84fac60b95b06928d39bceb4ebab161f
Marco Neumann <marco.neumann@blue-yonder.com>
2017-10-12 13:23:57 +0200

    [fish] add fisherman and nitro

--- modules/fish/setup.sh
+++ modules/fish/setup.sh
@@ -2,6 +2,15 @@
 
 set -euo pipefail
 
+# get fisherman
+curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
+
+# remove old config.fish
+rm -rf ~/.config/fish/config.fish
+
 # link config.fish
 mkdir -p ~/.config/fish
 ln -s $mdir/config.fish ~/.config/fish/config.fish
+
+# install fisherman plugins
+fish -c "fisher nitro"
