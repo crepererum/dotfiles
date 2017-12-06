a7e49639dd3324dff55fb7a91e2989babee440e7
Marco Neumann <marco@crepererum.net>
2017-12-06 23:09:04 +0100

    [fish] install fisherman after cfg setup

--- modules/fish/setup.sh
+++ modules/fish/setup.sh
@@ -2,15 +2,15 @@
 
 set -euo pipefail
 
-# get fisherman
-curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-
 # remove old config.fish
 rm -rf ~/.config/fish/config.fish
 
 # link config.fish
 mkdir -p ~/.config/fish
 ln -s $mdir/config.fish ~/.config/fish/config.fish
+
+# get fisherman
+curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
 
 # install fisherman plugins
 fish -c "fisher metro limeth/jellyfish"
