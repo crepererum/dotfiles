b2dddd38692915b8fd6a3d03bcb55c6ec337f4b9
Marco Neumann <marco.neumann@blue-yonder.com>
2018-05-16 10:26:40 +0200

    [git] remove webui

--- modules/git/setup.sh
+++ modules/git/setup.sh
@@ -8,7 +8,6 @@ rm -rIf ~/.gitconfig ~/.gitignore
 # link .gitconfig and .gitignore
 ln -s $mdir/gitconfig ~/.gitconfig
 ln -s $mdir/gitignore ~/.gitignore
-git clone https://github.com/alberthier/git-webui.git ~/.git-webui
 
 # create hooks if not exist
 mkdir -p ~/.githooks
