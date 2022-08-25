e7ca92bfe647bc410ecefde839abae9d5df851fa
Marco Neumann <marco@crepererum.net>
2022-08-25 13:37:34 +0200

    fish: remove TERM/COLORTERM hacks

--- modules/fish/config.fish
+++ modules/fish/config.fish
@@ -1,14 +1,3 @@
-# proper terminal variables
-# this is especially required on gnome terminal
-switch $TERM
-    case xterm
-        set -x TERM xterm-256color
-        set -x COLORTERM xterm-256color
-    case screen
-        set -x TERM screen-256color
-        set -x COLORTERM screen-256color
-end
-
 # default editor
 set -x EDITOR nvim
 
