dccd6b0a71448533d53fe7ef9dda5cc9ac0c9314
Marco Neumann <marco@crepererum.net>
2022-08-25 13:46:02 +0200

    vim: redesign for neovim

--- modules/vim/setup.sh
+++ modules/vim/setup.sh
@@ -8,25 +8,13 @@ rm -rIf ~/.vimrc ~/.viminfo ~/.vim ~/.config/nvim
 # create nvim config dir
 mkdir -p ~/.config/nvim
 
-# link .vimrc and nvimrc
-ln -s $mdir/vimrc ~/.vimrc
-ln -s ~/.vimrc ~/.config/nvim/init.vim
+# link configs
+ln -s $mdir/init.vim ~/.config/nvim/init.vim
 
 # create view dirs
-mkdir -p ~/.vim/view
 mkdir -p ~/.config/nvim/view
-
-# install vim-plug (vim)
-mkdir -p ~/.vim/autoload
-curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall +qa
 
 # install vim-plug (nvim)
 mkdir -p ~/.config/nvim/autoload
 curl -fLo ~/.config/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +qa
-
-# install YouCompleteMe
-#cd ~/.vim/bundle/YouCompleteMe
-#./install.sh --clang-completer
-
