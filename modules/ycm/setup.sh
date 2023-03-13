802d6d1cd888883b281af0f78f0c70252bb055d3
Marco Neumann <marco@crepererum.net>
2023-03-13 13:47:01 +0100

    ycm: remove

--- modules/ycm/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-# cleanup
-rm -rIf ~/.ycm_extra_conf.py ~/.ycm_extra_conf.pyc
-
-# link .ycm_extra_conf.py
-ln -s $mdir/ycm_extra_conf.py ~/.ycm_extra_conf.py
